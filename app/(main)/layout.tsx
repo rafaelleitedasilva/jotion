0960afe41c0de572e711508a6780c2d6d8385825
rafaelleitedasilva <rafael.leite.14@hotmail.com>
2023-11-24 09:47:04 -0300

    Fix LogOut: Fixing Client Side Error when the redirect was triggered

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { useConvexAuth } from "convex/react";
+import { useEffect } from "react";
+// import { useRouter } from "next/router";
 import { Spinner } from "@/components/spinner";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { Navigation } from "./_components/navigation";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
-    const { isAuthenticated, isLoading} = useConvexAuth();
+    const { isAuthenticated, isLoading } = useConvexAuth();
 
-    if(isLoading){
+    if (isLoading) {
         return (
             <div className="h-full flex items-center justify-center">
                 <Spinner size="lg" />
@@ -16,16 +18,20 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
         )
     }
 
-    if(!isAuthenticated){
-        return redirect("/");
-    }
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!isLoading && !isAuthenticated) {
+            router.push("/");
+        }
+    }, [isLoading, isAuthenticated, router]);
 
     return (
         <div className="h-full flex dark:bg-[#1F1F1F]">
-            <Navigation/>
+            <Navigation />
             <main className="flex-1 h-full overflow-y-auto">
 
-            {children}
+                {children}
 
             </main>
         </div>
