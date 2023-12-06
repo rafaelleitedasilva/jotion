ef0b675082ae759924cdd2dd02c022efd5851d90
rafaelleitedasilva <rafael.leite.14@hotmail.com>
2023-12-06 08:41:09 -0300

    Feat: Navbar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { ChevronsLeft, MenuIcon, Plus, PlusCircle, PlusIcon, Search, Settings, Trash } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts";
 import { cn } from "@/lib/utils";
@@ -19,12 +19,14 @@ import { Item } from "./item";
 import { toast } from "sonner";
 import { DocumentList } from "./document-list";
 import { TrashBox } from "./trash-box";
+import { Navbar } from "./navbar";
 
 export const Navigation = () => {
     const pathname = usePathname();
     const search = useSearch();
     const isMobile = useMediaQuery("(max-width: 768px)");
     const settings = useSettings();
+    const params = useParams();
 
     const create = useMutation(api.documents.create);
 
@@ -166,6 +168,13 @@ export const Navigation = () => {
             className="opacity-0 group-hover/sidebar:opacity-100 transition cursos-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"/>
         </aside>
         <div ref={navbarRef} className={cn("absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]", isResetting && "transition-all ease-in-out duration-300", isMobile && "left-0 w-full")}>
+            {!!params.documentId?(
+                <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth}/>
+            ):(
+                <nav className="bg-transparent px-3 py-2 w-full">
+                    {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground"  />}
+                </nav>
+            )}
             <nav className="bg-transparent px-3 py-2 w-full">
                 {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground"/>}
             </nav>
